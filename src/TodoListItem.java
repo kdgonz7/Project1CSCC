9d52d4b9a68862ce5180f8a9495d23d2b0e403de
Kai D. Gonzalez <gkai70263@gmail.com>
2026-02-12 20:40:48 -0500

    Revert "Create Project 2"
    This reverts commit 6e425a2bd1945970e232380df65f626b7c71f4b5.

--- src/TodoListItem.java
+++ src/TodoListItem.java
@@ -1,32 +1,10 @@
 public class TodoListItem {
-    String title;
     String description;
     boolean isCompleted;
-    int priority = 0; // 0 = no/low, 5 = high
 
     public TodoListItem(String description) {
         this.description = description;
         this.isCompleted = false;
-        this.priority = 0;
-    }
-
-    // keep backward compat with old constructor
-    public TodoListItem(String description, String title, int priority) {
-        this.title = title;
-        this.description = description;
-        this.isCompleted = false;
-        this.priority = priority;
-    }
-
-
-    public void setPriority(int number) {
-        if (number < 0) {
-            this.priority = 0;
-        } else this.priority = Math.min(number, 5);
-    }
-
-    public int getPriority() {
-        return priority;
     }
 
     public void markAsCompleted() {
@@ -35,6 +13,6 @@ public class TodoListItem {
 
     @Override
     public String toString() {
-        return (isCompleted ? "[x] " : "[ ] ") + title + "\n\t" + description;
+        return (isCompleted ? "[x] " : "[ ] ") + description;
     }
 }
